676b4f18fbb85ea2f9d9d27f5da0b8954416d684
Vior_Tula <36102143+viordash@users.noreply.github.com>
2017-07-30 12:48:34 +0300

    fix  implicit declaration of CalcCrc32

--- PSoC_Creator/PSU_PSoC5/PowerSuplyUnit.cydsn/Storage.h
+++ PSoC_Creator/PSU_PSoC5/PowerSuplyUnit.cydsn/Storage.h
@@ -1,7 +1,7 @@
-#ifndef __CRC_H
-#define __CRC_H
+#ifndef __CRC_UNIT_H
+#define __CRC_UNIT_H
 
 extern void LoadFromStorage();
 extern void SaveToStorage();
 
-#endif /* end __CRC_H */
+#endif /* end __CRC_UNIT_H */
